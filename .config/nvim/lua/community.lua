7902052a31518a3404cb87b1e3877f0457ff09c4
Alexandre Barsacq <46349802+alexandrebarsacq@users.noreply.github.com>
2025-03-05 11:26:40 +0100

    nvim update

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -10,13 +10,26 @@ return {
   { import = "astrocommunity.pack.cpp" },
   -- { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.recipes.heirline-clock-statusline" },
+
+  -- vim sneak ++ (use 's')
   { import = "astrocommunity.motion.leap-nvim" },
+
   { import = "astrocommunity.scrolling.neoscroll-nvim" },
   { import = "astrocommunity.git.gitgraph-nvim" },
+
+  -- diffview for git stuff
   { import = "astrocommunity.git.diffview-nvim" },
+
   { import = "astrocommunity.syntax.vim-easy-align" }, -- Easyalign *,
   -- { import = "astrocommunity.debugging.persistent-breakpoint-nvim" },
+  --
+  -- -- shows the variable content inline, while debugging
   { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+
+  { import = "astrocommunity.motion.grapple-nvim" },
+
+  { import = "astrocommunity.diagnostics.tiny-inline-diagnostic-nvim" },
+
   -- { import = "astrocommunity.nvim-dap-repl-highlights" },
   -- TODO
   -- { import = "astrocommunity.nvim-chansaiw" },
--- .config/nvim/lua/plugins/nvim-dap-python.lua
+++ .config/nvim/lua/plugins/nvim-dap-python.lua
@@ -1,11 +1,42 @@
 ---@type LazySpec
 return {
-
-  -- == Examples of Adding Plugins ==
   {
     "mfussenegger/nvim-dap-python",
-    config = function() require("dap-python").setup "python3.12" end,
-  },
+    config = function()
+      -- override existing confs to set cwd and env to project root so
+      -- that a project where i run files in a hierarchy,
+      -- the imports of other files of the project,
+      -- relative to project root, are handled correctly
+      local nvim_lsp = require "lspconfig"
+      require("dap-python").setup "python3.12"
 
-  -- cwd = function() return util.root_pattern "pyproject.toml"(vim.fn.getcwd()) end,
+      local function get_project_root() return nvim_lsp.util.root_pattern ".git"(vim.fn.getcwd()) end
+
+      local function update_config(config)
+        ---@cast config table  -- Override strict type checking
+        config.cwd = get_project_root
+        config.env = { PYTHONPATH = get_project_root }
+      end
+
+      local dap = require "dap"
+      if dap.configurations.python then
+        for _, config in ipairs(dap.configurations.python) do
+          update_config(config)
+        end
+      end
+
+      -- custom conf, subject to change depending on my daily needs
+      table.insert(require("dap").configurations.python, {
+        type = "python",
+        request = "launch",
+        name = "My custom launch configuration",
+        program = "/home/alexandre/code/dsp-outils/dspy/sensor_characterization/verify_hr_perfo_req.py",
+        cwd = function() return nvim_lsp.util.root_pattern ".git"(vim.fn.getcwd()) end,
+        env = {
+          PYTHONPATH = function() return nvim_lsp.util.root_pattern ".git"(vim.fn.getcwd()) end,
+        },
+        -- ... more options, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings
+      })
+    end,
+  },
 }
